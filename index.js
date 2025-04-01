ab5d63a82041f3a63b1a7841e79295ede5b3cd2f
diagne <diagnebuyengo@gmail.com>
2025-04-02 00:01:07 +0300

    event listening

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
 function addingEventListener() {
+    
+    const button = document.getElementById("button");
+
+
+    button.addEventListener("click", function() {
+        console.log("Button clicked!");
+    });
 }
+
+
+addingEventListener();
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 10}px`; 
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
